7c2ac972a00eaa4b4ae943d96ec27f58a7b49ffd
Jaortiz1 <juanantonioortizguerra@gmail.com>
2022-06-03 09:21:30 +0200

    actualizo domain model

--- src/main/java/acme/entities/chimpum/Chimpum.java
+++ src/main/java/acme/entities/chimpum/Chimpum.java
@@ -38,7 +38,7 @@ public class Chimpum extends AbstractEntity {
 	protected static final long	serialVersionUID	= 1L;
 	@NotBlank
 	@Pattern(regexp = "^\\d{4}\\-(0[1-9]|1[012])\\-(0[1-9]|[12][0-9]|3[01])$")
-	@Column(unique = true)
+	@Column(unique = false)
 	protected String			code;
 
 	@NotBlank
